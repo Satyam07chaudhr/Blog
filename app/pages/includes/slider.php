b412ddb71388dddd6a219f44e9a5306403e7707d
Satyam07chaudhr <149891540+Satyam07chaudhr@users.noreply.github.com>
2024-09-12 15:35:09 +0530

    Add files via upload

---
+++ app/pages/includes/slider.php
@@ -0,0 +1,26 @@
+
+  <!-- slider -->
+  <link rel="stylesheet" href="<?=ROOT?>/assets/slider/ism/css/my-slider.css"/>
+  <script src="<?=ROOT?>/assets/slider/ism/js/ism-2.2.min.js"></script>
+
+  <div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
+    <ol>
+      <li>
+        <img src="<?=ROOT?>/assets/images/first.jpg">
+        <div class="ism-caption ism-caption-0">My slide caption text</div>
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/images/2.jpg">
+        <div class="ism-caption ism-caption-0">My slide caption text</div>
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/images/sa.jpg">
+        <div class="ism-caption ism-caption-0">My slide caption text</div>
+      </li>
+      <li>
+        <img src="<?=ROOT?>/assets/images/ra.jpg">
+        <div class="ism-caption ism-caption-0">My slide caption text</div>
+      </li>
+    </ol>
+  </div>
+  <!-- end slider -->
